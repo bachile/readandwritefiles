bb0bcee985f37d96385bb62a3381e19a2230c694
Brando <brando_lezzana1@baylor.edu>
2024-02-08 19:51:39 -0600

    changed employee report

--- employee_report.py
+++ employee_report.py
@@ -8,14 +8,57 @@ def main():
 
     next(csv_file)
 
+    efficient = []
+    inefficient = []  
+
+    fourties = []
+    thirties = []
+    twenties = []
+
     for rec in csv_file:
 
-        productivity = rec[5]
-        hours = rec[4]
+        name = rec[1]
+        age = int(rec[2])
+        productivity = int(rec[5])
+        hours = int(rec[4])
 
         efficiency = productivity/hours
 
-        if efficiency >= 2:
-            
+        if efficiency > 2:
+            efficient.append(name)
+        if efficiency < 1:
+            inefficient.append(name)
+
+        if age >= 40:
+            fourties.append(name)
+        elif age >= 30:
+            thirties.append(name)
+        else:
+            twenties.append(name)
+
+    print('\nHighly Efficient Individuals:')
+    for person in efficient:
+        print(person)
+
+    print('\n\nLow Efficiency Individuals:')
+    for person in inefficient:
+        print(person)
+
+    print('\n\nEmployees in their 40s:')
+    for person in fourties:
+        print(person)
+    print('\nTotal number of emplyees in their 40s:', len(fourties))
+
+    print('\n\nEmployees in their 30s:')
+    for person in thirties:
+        print(person)
+    print('\nTotal number of emplyees in their 30s:', len(thirties))
+
+    print('\n\nEmployees in their 20s:')
+    for person in twenties:
+        print(person)
+    print('\nTotal number of emplyees in their 20s:', len(twenties))
+        
+    print()
 
 main()
